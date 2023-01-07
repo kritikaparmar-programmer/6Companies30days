4ea15bdd30741e1459929372d10ac63e18118dd2
Kritika Parmar <59971890+kritikaparmar-programmer@users.noreply.github.com>
2023-01-07 14:54:27 +0000

    Day 7

--- Goldman_Sachs/06_MinConsecutiveCardsToPick.py
+++ Goldman_Sachs/06_MinConsecutiveCardsToPick.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/
+
+# my sol
 class Solution:
     def minimumCardPickup(self, cards) -> int:
         myMap = {}
@@ -13,3 +15,16 @@ class Solution:
         if len(ans) == 0: return -1
         ans.sort()
         return ans[0]     
+
+
+# other way
+    def minimumCardPickup2(self, cards) -> int:
+        lastOcc = [-1] * (int(1e7))
+        ans = 1e9
+
+        for i in range(len(cards)):
+            if lastOcc[cards[i]] != -1:
+                ans = min(i - lastOcc[cards[i]]  + 1, ans)
+            lastOcc[cards[i]] = i
+
+        return -1 if ans == 1e9 else ans
